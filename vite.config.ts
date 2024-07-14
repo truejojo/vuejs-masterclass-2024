d0e5b50ee44a19141503db52592dbe10c2e5f684
truejojo <johannesveh@yahoo.de>
2024-07-14 13:04:30 +0200

    tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,17 @@ import VueRouter from 'unplugin-vue-router/vite';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
+import tailwind from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [VueRouter(), vue()],
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()]
+    }
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
